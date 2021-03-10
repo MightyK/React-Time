13b9a33e9e6a5db70ffb777d87a474c29850da05
MightyK <KMightyDev@gmail.com>
2021-03-10 13:25:44 -0500

    Initial App setup

---
+++ src/Components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div className='container'>
+            <h1>TIME</h1>
+            <button>Get Time</button>
+        </div>
+    );
+}
+
+export default App;
